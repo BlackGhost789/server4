7822695588beef21cb6412a5ac138b830ea1776c
AmineTouati <128828801+BlackGhost789@users.noreply.github.com>
2024-06-13 14:21:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+
+let i = 0;
+
+app.get('/init', (req, res)=> {
+    setInterval(async () => {
+        const response2 = await axios.get('https://informatique-dz-api.onrender.com/');
+        i++
+        console.log(i);
+        console.log('iterated')
+    }, 100000);
+})
+
+
+app.listen(3000, ()=>{
+    console.log('server listening')
+})
